174c5774898c9d2fcbe8d3568c513d36b7c55087
Hamid <HamidSaliqa>
2024-02-08 14:13:00 +0330

    افزودن  دایرکتوری ویجت و انتقال ویجت ها به ان

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,5 @@
-import 'package:animated_text_kit/animated_text_kit.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_share/flutter_share.dart';
-import 'package:lottie/lottie.dart';
+import 'package:top_seven/widget.dart';
 import 'second_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -65,6 +62,7 @@ class CustomButton extends StatelessWidget {
     );
   }
 
+  /// on pressed
   forOnpressed(BuildContext context) {
     if (nameOfLibrary == "Lottie") {
       Navigator.push(
@@ -87,69 +85,13 @@ class CustomButton extends StatelessWidget {
           builder: (context) => SeconPage(yourWidget: mytext()),
         ),
       );
-    }else if (nameOfLibrary == "carousel_slider") {
+    } else if (nameOfLibrary == "carousel_slider") {
       Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => SeconPage(yourWidget: Slider()),
+          builder: (context) => SeconPage(yourWidget: slider()),
         ),
       );
     }
   }
 }
-
-const labelTextStyle = TextStyle(
-  fontSize: 20,
-  fontWeight: FontWeight.bold,
-);
-
-Widget myLottie() {
-  return Lottie.asset("assets/animation/Animation1json");
-}
-
-Widget myShareButton() {
-  return ElevatedButton(
-      onPressed: () async {
-        await FlutterShare.share(title: "Share");
-      },
-      child: Text("Click Me"));
-}
-
-Widget mytext() {
-  return SizedBox(
-    width: 250.0,
-    child: DefaultTextStyle(
-      style: const TextStyle(
-          fontSize: 40.0, fontFamily: 'Canterbury', color: Colors.black),
-      child: AnimatedTextKit(
-        animatedTexts: [
-          ScaleAnimatedText('Welcome'),
-          ScaleAnimatedText('it is me animated_text_kit'),
-          ScaleAnimatedText('Good Bye'),
-        ],
-        onTap: () {},
-      ),
-    ),
-  );
-}
-
-Widget Slider (){
-  return CarouselSlider(
-    options: CarouselOptions(height: 400.0),
-    items: [1,2,3,4,5].map((i) {
-      return Builder(
-        builder: (BuildContext context) {
-          return Container(
-              width: MediaQuery.of(context).size.width,
-              margin: EdgeInsets.symmetric(horizontal: 5.0),
-              decoration: BoxDecoration(
-                  color: Colors.deepPurple[900]
-              ),
-              child: Center(child: Text('text $i', style: TextStyle(fontSize: 25.0,color: Colors.white),))
-          );
-        },
-      );
-    }).toList(),
-  );
-}
-
